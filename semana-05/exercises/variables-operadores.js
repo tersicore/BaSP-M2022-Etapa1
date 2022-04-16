a61dc773a98bcc688baa92deeef26f40cffc8aae
tersicore <ade.beinlich@gmail.com>
2022-04-16 11:42:19 -0300

    BaSP2022: adding files

---
+++ semana-05/exercises/variables-operadores.js
@@ -0,0 +1,10 @@
+
+//Crear dos variables numéricas y utilizar el operador suma para guardar el valor de la suma de ambos números en una 3er variable.
+
+var numero1 = 900;
+var numero2 = 485.5
+
+var sumaNumeros = numero1 + numero2 
+
+
+
