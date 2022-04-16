3fee2e084310bd02e7a4d829d93e4b11c10961a1
tersicore <ade.beinlich@gmail.com>
2022-04-16 13:23:47 -0300

    BaSP2022: Ex 1

--- semana-05/exercises/variables-operadores.js
+++ semana-05/exercises/variables-operadores.js
@@ -2,9 +2,26 @@
 //Crear dos variables numéricas y utilizar el operador suma para guardar el valor de la suma de ambos números en una 3er variable.
 
 var numero1 = 900;
-var numero2 = 485.5
+var numero2 = 485.5;
 
-var sumaNumeros = numero1 + numero2 
+var sumaNumeros = numero1 + numero2;
 
+console.log(sumaNumeros);
 
+//Crear dos variables de tipo String y concatenarlas guardando el resultado en una 3er variable.
 
+var string1 = "Hola "
+var string2 = "amigo"
+
+var frase = string1 + string2 
+
+console.log(frase)
+
+//Crear dos variables de tipo String y sumar el largo de cada variable (cantidad de letras del string) guardando el resultado de la suma en una 3er variable (utilizar length).
+
+var string3 = "Mi perra "
+var string4 = "ladra" 
+
+var sumaString = string3.length + string4.length
+
+console.log(sumaString)
