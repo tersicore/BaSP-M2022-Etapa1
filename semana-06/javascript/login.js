8bcff7f66420dad97daf48eb3aeebe487fe1c0d7
tersicore <ade.beinlich@gmail.com>
2022-04-26 21:22:10 -0300

    emergencia

--- semana-06/javascript/login.js
+++ semana-06/javascript/login.js
@@ -1,6 +1,7 @@
 
     
 window.onload = function() {
+      
 
 
     var FormLogIn = document.getElementById("formLogIn");
@@ -15,11 +16,12 @@ window.onload = function() {
     
     function validate(e){
 
-    e.preventDefault();
+ e.preventDefault();
+
     var email = document.getElementById("mail").value;
     var expMail = /\w+@\w+\.+[a-z]/;
     var password = document.getElementById("passwordLogIn").value;
-    var expPassword = /^(?=\w*\d)(?=\w*[A-Z])(?=\w*[a-z])\S{8,16}$/
+    //var expPassword = /^(?=\w*\d)(?=\w*[A-Z])(?=\w*[a-z])\S{8,16}$/
 
         if(email === "") {
             alert("insert email");
@@ -30,22 +32,78 @@ window.onload = function() {
             alert("email error");
             return false;
         }
+        if (password === ""){
+            alert("insert passwors");
+            return false;
+         }
 
-            if (!expPassword.test(password))
-        {
-            alert("debe tener numerods y letras, y un maximo de 8 caracteres ");
+        if (!validNumLetr(password)){
+            alert("password must contain letters and numbers ");
             return false;
         }
 
-            if (password === ""){
-            alert("Insert password");
-            return false;
+        if (password.length < 7) {
+            alert("passwors must contain 8 characters or more")
         }
+
+        
         else{
             alert("Enviados")
         }
-        return validate
+    return validate
         }
+
+
+    var ValidLetra = function(prueba) {
+ 
+
+    for(var i = 0; i <prueba.length; i++ ) {
+
+      
+
+        if ((prueba[i] >= "a" && prueba[i] <= "z") || prueba[i] >= "A" && prueba[i] <= "Z")  {
+             
+        }
+        else { return false}
+
+    }
+    return true
+} 
+
+
+var ValidNum = function(mys) {
+
+    var numArray = ["0","1","2","3","4","5","6","7","8","9"]
+
+   for( var i =0 ; i < mys.length; i++ ) {
+       if(!numArray.includes(mys[i])){
+           return false
+       }
+   }
+
+   return true
+
+}
+
+
+var validNumLetr = function(muy) {
+    var letra = false 
+    var numero = false 
+    for (var i = 0; i< muy.length;i++ ){
+        if(ValidNum(muy[i])){
+            numero = true
+        }
+        else if(ValidLetra(muy[i])){
+            letra = true
+        }
+        else{
+            return false
+        }
+    }
+   return letra && numero
+}
+
+
 
 
     
@@ -56,13 +114,13 @@ window.onload = function() {
             var email = document.getElementById("mail").value;
             var expMail = /\w+@\w+\.+[a-z]/;
             var password = document.getElementById("passwordLogIn").value;
-            var expPassword = /^(?=\w*\d)(?=\w*[A-Z])(?=\w*[a-z])\S{8,16}$/;
+            //var expPassword = /^(?=\w*\d)(?=\w*[A-Z])(?=\w*[a-z])\S{8,16}$/;
             var passwordError = document.getElementById("errorPassword");
 
             if(email === "" || !expMail.test(email) ) {
             emailError.style.display="block";
         }
-             if(!expPassword.test(password) || password === "" ){
+             if(!validNumLetr(password) || password === "" ){
             passwordError.style.display="block";
         }
         return false;
---
+++ semana-06/javascript/sing-up.js
@@ -0,0 +1,227 @@
+window.onload = function() {
+
+var formSignUp = document.getElementById("formSignUp");
+formSignUp.addEventListener("submit",validateS )
+
+var name = document.getElementById("name").value;
+
+function validateS(e) {
+ 
+    e.preventDefault();
+
+    var name = document.getElementById("name").value;
+    var lastname =document.getElementById("lastname").value;
+    var DNI =document.getElementById("DNI").value;
+    var telephone = document.getElementById("telephone").value
+    var Bdate = document.getElementById("Bdate").value
+    var postalCode = document.getElementById("postalCode").value
+    var address =document.getElementById("address").value
+    var locality = document.getElementById("locality").value
+    var emailSignUp = document.getElementById("mailS").value
+    var passwordSignUp =document.getElementById("passwordSignUp").value
+    //var ExpSoloLetras = /^[A-Za-z]/;
+    var expMail = /\w+@\w+\.+[a-z]/;
+    //var expPassword = /^(?=\w*\d)(?=\w*[A-Z])(?=\w*[a-z])\S{8,16}$/
+    
+
+
+    if(name === "") {
+        alert("insert name");
+        return false;
+    }
+     if (name.length < 3){
+        alert("name must contain more than 3 characters")
+        return false
+        }
+    if(ValidLetra(name)){
+        alert("name must contain only letters");
+        return false;
+        }
+     if (lastname === ""){
+        alert("insert last name");
+        return false;
+        }
+     if (lastname.length < 3){
+        alert("lastname must contain more than 3 characters")
+        return false
+        }
+    if(ValidLetra(lastname)){
+        alert("lastname must contain only letters");
+        return false;
+        }
+
+    if(DNI === "") {
+            alert("insert DNI");
+            return false;
+        }
+    if( ValidNum(DNI)) {
+            alert("DNI must be a number");
+            return false;
+        }
+    if (DNI.length < 7) {
+        alert("DNI must contain more tha 7 characters")
+        return false;
+    }
+    if(telephone === "") {
+            alert("insert Telephone");
+            return false;
+        }
+    if( ValidNum(telephone)) {
+            alert("Telephone must be a number");
+            return false;
+        }
+    if (telephone.length < 9) {
+        alert("Telepghone must contain more than 10 characters")
+        return false;
+    }
+
+    if (Bdate === ""){
+        alert("insert date")
+        return false;
+    }
+
+    if(postalCode === "") {
+            alert("insert Postal Code");
+            return false;
+        }
+    if(ValidNum(postalCode)) {
+            alert("Postal Code must be a number");
+            return false;
+        }
+    if ( !(postalCode.length == 4 || postalCode.length ==5) ) {
+        alert("Postal code must haver 4 or 5 characters")
+        return false;
+    }
+
+     if (address === ""){
+        alert("insert address");
+        return false;
+        }
+     if (address.length < 4){
+        alert("address must contain more than 5 characters")
+        return false
+        }
+
+       /*if (address = 'string' + '/s' + 'string')   {
+        alert("caca")
+        return false
+        }*/
+
+
+    
+    if (locality === ""){
+        alert("insert locality");
+        return false;
+        }
+     if (locality.length < 3){
+        alert("locality must contain more than 3 characters")
+        return false
+        }
+
+     if(emailSignUp === "") {
+            alert("insert email");
+            return false;
+        }
+
+    if (!expMail.test(emailSignUp)) {
+            alert("email error");
+            return false;
+        }
+
+    if (passwordSignUp === ""){
+            alert("Insert password");
+            return false;
+    }
+    if (!validNumLetr(passwordSignUp)){
+            alert("password must contain numbers and letters");
+            return false;
+    
+    }
+
+    if (ValidLetra(passwordSignUp)){
+        alert("tiene solo letras");
+        return false;
+    }
+
+
+
+    if (passwordSignUp.length < 7){
+        alert("password must contain at least 8 characters or more")
+        return false
+    }
+
+     else{
+        alert("enviado")
+    }
+
+return validateS
+
+}
+
+
+
+
+
+var ValidLetra = function(prueba) {
+ 
+
+    for(var i = 0; i <prueba.length; i++ ) {
+
+      
+
+        if ((prueba[i] >= "a" && prueba[i] <= "z") || prueba[i] >= "A" && prueba[i] <= "Z")  {
+             
+        }
+        else { return false}
+
+    }
+    return true
+} 
+
+var probando = ValidLetra("kfkf")
+
+console.log(probando)
+
+
+var ValidNum = function(mys) {
+
+    var numArray = ["0","1","2","3","4","5","6","7","8","9"]
+
+   for( var i =0 ; i < mys.length; i++ ) {
+       if(!numArray.includes(mys[i])){
+           return false
+       }
+   }
+
+   return true
+
+}
+
+//console.log(ValidNum("f565"))
+
+
+
+
+//probando = ValidLetraNum("fgfd")
+//console.log(probando)
+
+var validNumLetr = function(muy) {
+    var letra = false 
+    var numero = false 
+    for (var i = 0; i< muy.length;i++ ){
+        if(ValidNum(muy[i])){
+            numero = true
+        }
+        else if(ValidLetra(muy[i])){
+            letra = true
+        }
+        else{
+            return false
+        }
+    }
+   return letra && numero
+}
+
+
+
+}
